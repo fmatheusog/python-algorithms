ba6b833d9e9cf7e33e12bbe0e3775fab25703957
Felipe Matheus <fmatheusog@gmail.coM>
2022-09-05 18:06:15 -0300

    feat: req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if word is None or not word:
+        return False
+
+    while low_index <= high_index:
+        if word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        else:
+            return False
+
+    return True
